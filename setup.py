1e2a00d8b535d877840033f805e0a1600bb89690
aGoatdev <crazytekno.py@gmail.com>
2023-09-28 13:33:14 +0200

    Setup Script

--- setup.py
+++ setup.py
@@ -2,13 +2,21 @@ from setuptools import find_packages, setup, Command
 
 with open("mcpypanel/__version__.py", "r") as f:
     exec(f.read())
+    
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setup(
     name='McPyPanel',
     version=__version__,
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
+    description="A panel for minecraft servers and proxy, doc at https://github.com/aGoatdev/mcpypanel"
+    packages=["mcpypanel"],
     license='',
+    author="aGoatdev"
+    author_emaik="crazytekno.py@gmail.com"
     long_description=open('README.md').read(),
-    include_package_data=True,
-    scripts=["bin/mcwrapper"]
+    url="https://github.com/aGoatdev/mcpypanel" # Project Url
+    scripts=["bin/mcpypanel"], # Start Script
+    install_requires=required, # Requirements
+    platforms="linux",
 )
