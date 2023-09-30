0cdb7b050a1f31eb40a4df0e7d6c1ebfee50864e
Bob (Léo et Claire) <crazytekno.py@gmail.com>
2023-09-30 09:01:53 +0200

    d

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # mcpypanel
-Just a panel for a minecraft Server / Proxy, writen with python.
+One of the best open-source panel for a minecraft Server / Proxy, writen with python.
+Admins, Servers owners or teens wanting simply a minecraft server: This is one of your best friends!
+
+You can easily control everything on a server / group of server (proxy).
+
 It is being coded with a linux machine.
 
-It is not working for the moment.
+It is not working for the moment. (I'm actively coding this masterpiece)
 
 For the list of planned features and features that are already in, please take a look at [TODO.md](TODO.md)
--- mcpypanel/minecraft/__init__.py
+++ mcpypanel/minecraft/__init__.py
@@ -1,18 +1,19 @@
 import uuid
 import nbtlib
+import time
+import os
+class Player:
+    def __init__(self,path,uuid):
+        self.path = path+"/playerdata"+str(uuid)
+        self.nbt = nbtlib.load(self.path+"/level.data")
 
-class WorldNbt:
-    def __init__(self,dir,name="level.nbt"):
-        self.path = path+"/"+name
-        self.file = nbtlib.load(self.path))
-    def __getattr__(self,attr):
-        return self.file[attr]
-    def __setattr__(self,attr,data):
-        self.file[attr]=data
-        self.file.save()
-    def __delattr__(self,attr):
-        raise NotImplementedError
 class World:
     def __init__(self,dir,world_name):
         self.path = dir+"/"+world_name
-        self.nbt = WorldNbt(self.path)
+        self.nbt = nbtlib.load(self.path+"/level.dat")
+        self.players = {}
+    def _refresh(self):
+        for u in os.listdir(self.path+"/playerdata"):
+            u = uuid.UUID(u)
+            self.players[u] = Player(path,u)
+            
