8967d29603d342637925463f43c6648418561c51
Bob (Léo et Claire) <crazytekno.py@gmail.com>
2023-10-08 20:09:56 +0200

    Wizard and Console went high

--- mcpypanel/control/console.py
+++ mcpypanel/control/console.py
@@ -78,16 +78,7 @@ class Console:
 """   
 
 
-BANNER ="""
-_  _   ___  ____  _  _  ____   __   __ _  ____  __    
-( \/ ) / __)(  _ \( \/ )(  _ \ / _\ (  ( \(  __)(  )  
-/ \/ \( (__  ) __/ )  /  ) __//    \/    / ) _) / (_/\\
-\_)(_/ \___)(__)  (__/  (__)  \_/\_/\_)__)(____)\____/"""
-def _BANNER_SIZE():
-    max = ""
-    for i in BANNER.split("\n"):
-        if len(i) > len(max):max = i
-    return (len(max),len(BANNER.split("\n")))
+
 
 
 
