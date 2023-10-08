e15faeb4cfc4506c1b276597e67778e549632ae1
Bob (Léo et Claire) <crazytekno.py@gmail.com>
2023-10-08 19:20:33 +0200

    Update README.md

--- mcpypanel/control/console.py
+++ mcpypanel/control/console.py
@@ -1,8 +1,13 @@
-import curses
-from curses.textpad import Textbox, rectangle
+# import curses
+# from curses.textpad import Textbox, rectangle
+import colorama
 import sys,os
 
+colorama.init()
 
+# Test of a console using curses
+
+"""
 class Console:
     def __init__(self,):
         ################################
@@ -41,12 +46,12 @@ class Console:
             elif key in [curses.KEY_UP,curses.KEY_DOWN,curses.KEY_LEFT,curses.KEY_RIGHT]:
                 curses.beep()
                 #self._handle_keys(key)
-            """
+            \"""
             elif key == curses.KEY_RESIZE:
                 curses.resize_term(*self.stdscr.getmaxyx())
                 self.stdscr.clear()
                 self._resize()
-            """
+            \"""
             self.stdscr.refresh()
             
     def _init(self):
@@ -62,16 +67,23 @@ class Console:
         curses.doupdate()
                 
     def write(self,text,window=None):
-        """
+        \"""
         Write text to the current screen.
         
         Can be used as a waky solution, with print:
             print("Hello world",file=console)
-        """
+        \"""
         if window is None:
             window = self.windows["log"]
-            
-            
+"""   
+
+class Console:
+    def __init__(self,parent):
+        self.parent = parent
+        self._commands = {}
+        
+    def register_command(self,func,command):
+        self._commands[command] = func
             
             
 if __name__ == "__main__":
