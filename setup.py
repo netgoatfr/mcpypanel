d0e3ecd11b42d9b03b8f1b73be0aba1a1493e9d5
aGoatdev <crazytekno.py@gmail.com>
2023-10-11 20:35:23 +0200

    Corrected Opsis in setup.py :)

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import find_packages, setup, Command
 
-with open("mcpypanel/__version__.py", "r") as f:
+with open("src/mcpypanel/__version__.py", "r") as f:
     exec(f.read())
     
 with open('requirements.txt') as f:
     required = f.read().splitlines()
 
+
+##     packages=["mcpypanel","mcpypanel.*"],
+
 setup(
     name='McPyPanel',
     version=__version__,
     description="A panel for minecraft servers and proxy, doc at https://github.com/aGoatdev/mcpypanel",
-    packages=["mcpypanel","mcpypanel.*"],
     license='CC BY-SA',
     author="aGoatdev",
-    author_emaik="crazytekno.py@gmail.com",
+    author_email="crazytekno.py@gmail.com",
     long_description=open('README.md').read(),
     url="https://github.com/aGoatdev/mcpypanel", # Project Url
     scripts=["bin/mcpypanel"], # Start Script
