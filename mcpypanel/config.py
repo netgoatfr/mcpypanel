bc69f527ef9ec480ea910cb083574bb6b4ae42b9
aGoatdev <crazytekno.py@gmail.com>
2023-11-12 14:00:43 +0100

    I'm gonna kill myself :tired_face:

--- mcpypanel/config.py
+++ mcpypanel/config.py
@@ -18,7 +18,7 @@ class ConfFile:
             self.parent._abort_startup = True
             return
         elif  not os.path.exists(self._file) and self._create:
-            self._save()
+            self.save()
             
         with open(self._file) as f:
             self._datas = load(f)
@@ -30,19 +30,23 @@ class ConfFile:
         if not (type(index) in (str, bytes)):
             raise Exception("a str/bytes must be passed")
         return self._datas.get(index,Empty())
+        self.save()
     def __setitem__(self, index, value):
         if not (type(index) in (str, bytes)):
             raise Exception("a str/bytes must be passed")
         self._datas[index] = value
-        return self.data[index]
+        return self._datas[index]
+        self.save()
     def __delattr__(self, index):
         if not (type(index) in (str, bytes)):
             raise Exception("a str/bytes must be passed")
         del self._datas[index]
+        self.save()
     def __delitem__(self, index):
         if not (type(index) in (str, bytes)):
             raise Exception("a str/bytes must be passed")
         del self._datas[index]
+        self.save()
     def __iter__(self):
         for i in self._datas:
             yield i
